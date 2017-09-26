3fa4248c56c25bb471a8ae499b9a24bf01862697
Luis Jariego <luiscjariego@gmail.com>
2017-09-26 19:35:02 +0200

    Initial commit
    New django project

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("<a href='/rango/'>Index</a>")
