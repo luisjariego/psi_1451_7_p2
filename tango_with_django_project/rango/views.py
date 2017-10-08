2d4d4d1dfd5bbda001e861b761a2ac25994b69bf
David <david.nevadoc@estudiante.uam.es>
2017-10-09 01:14:54 +0200

    casi terminao

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    ##context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list,
+                    'boldmessage': "Crunchy, creamy, cooke, candy, cupcake!"}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
