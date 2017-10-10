2e2fdaef7d3b6fa5edf3686ccfc006418216f2d6
Luis <luiscjariego@gmail.com>
2017-10-10 19:42:31 +0200

    avansando

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -6,6 +6,8 @@ from rango import views
 urlpatterns = [
     url(r'^index/', views.index, name='index'), #Index
     url(r'^about/', views.about, name='about'), # About
-	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', #Category
+	url(r'^add_category/$', views.add_category, name='add_category'), #Category (add)
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', #Category (show)
         views.show_category, name='show_category'),
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'), #Page (add)
 ]
